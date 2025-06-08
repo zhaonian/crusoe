5a96f09abec5d962769defa51e5cc1f79abb975b
Zhao Luan <zluan@adobe.com>
2025-06-08 14:27:31 -0700

    Update app ID to io.zluan

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.crusoe"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "io.zluan.crusoe"
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -21,11 +21,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.crusoe"
+        applicationId = "io.zluan.crusoe"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
--- android/app/src/main/kotlin/io/zluan/crusoe/MainActivity.kt
+++ android/app/src/main/kotlin/io/zluan/crusoe/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.crusoe
+package io.zluan.crusoe
 
 import io.flutter.embedding.android.FlutterActivity
 
